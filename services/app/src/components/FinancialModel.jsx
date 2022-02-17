9d373652ecac8479855270b9b97fb8630120d1b3
krisolchova <kristina.olchova@yahoo.com>
2022-02-17 18:28:36 +0000

    toolbar show if currentUser

--- services/app/src/components/FinancialModel.jsx
+++ services/app/src/components/FinancialModel.jsx
@@ -5,11 +5,22 @@ import useFetchSpreadsheet from '../hooks/useFetchSpreadsheet'
 import { utils } from '@tracktak/common'
 import useSaveSpreadsheetData from '../hooks/useSaveSpreadsheetData'
 import { useParams } from 'react-router-dom'
+import { useAuth } from '@tracktak/common'
 
-const FinancialModel = ({ isReadOnly }) => {
+const FinancialModel = () => {
   const params = useParams()
+  const { userData } = useAuth()
   const spreadsheetData = useFetchSpreadsheet(params.sheetId)
   const saveSheetData = useSaveSpreadsheetData(spreadsheetData)
+  const objectUserData = userData?.identities
+    ? JSON.parse(userData.identities)
+    : null
+
+  const userIdentity = objectUserData
+    ? objectUserData[0]?.providerName + '_' + objectUserData[0]?.userId
+    : null
+
+  const isCurrentUser = spreadsheetData?.userId === userIdentity
 
   return (
     <>
@@ -22,7 +33,7 @@ const FinancialModel = ({ isReadOnly }) => {
       )}
       {spreadsheetData?.sheetData.data.sheets && (
         <FinancialSpreadsheet
-          isReadOnly={isReadOnly}
+          isReadOnly={!isCurrentUser}
           saveSheetData={saveSheetData}
           spreadsheetData={spreadsheetData}
           sx={{
